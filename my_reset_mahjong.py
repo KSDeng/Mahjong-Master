75161b2c5254d5fcb1898ad208ac239a1c4cee74
KSDeng <kaisheng.deng@outlook.com>
2022-10-26 16:07:27 +0800

    code for building resnet34

--- my_reset_mahjong.py
+++ my_reset_mahjong.py
@@ -13,6 +13,76 @@ def getTimeStamp():
     now = datetime.datetime.now()
     return now.strftime("%Y%m%d%H%M%S")
 
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride = 1, down_sample = None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(3,3), stride=(stride, stride), padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, kernel_size=(3,3), stride=(1,1), padding=1),
+            nn.BatchNorm2d(out_channels)
+        )
+        self.down_sample = down_sample
+        self.relu = nn.ReLU()
+        self.out_channels = out_channels
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.conv2(out)
+        if self.down_sample:
+            residual = self.down_sample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes = 10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=(7,7), stride=(2,2), padding=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+        self.max_pool = nn.MaxPool2d(kernel_size=(3,3), stride=(2,2), padding=1)
+        self.layer0 = self._make_layer(block, 64, layers[0], stride=1)
+        self.layer1 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer2 = self._make_layer(block, 256, layers[2], stride=1)
+        self.layer3 = self._make_layer(block, 512, layers[3], stride=1)
+        self.avg_pool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(2048, num_classes)
+
+    def _make_layer(self, block, planes, blocks, stride = 1):
+        down_sample = None
+        if stride != 1 or self.in_planes != planes:
+            down_sample = nn.Sequential(
+                nn.Conv2d(self.in_planes, planes, kernel_size=(1,1), stride=(stride,stride)),
+                nn.BatchNorm2d(planes)
+            )
+        layers = []
+        layers.append(block(self.in_planes, planes, stride, down_sample))
+        self.in_planes = planes
+        for i in range(1, blocks):
+            layers.append(block(self.in_planes, planes))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.max_pool(x)
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
 if __name__ == "__main__":
     image_size = (64, 64)
     weight_save_path = 'weight-resnet50-' + getTimeStamp()
@@ -47,7 +117,7 @@ if __name__ == "__main__":
 
     # training model
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    model = resnet50()
+    model = ResNet(ResidualBlock, [3,4,6,3], num_classes=34).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     scheduler = ExponentialLR(optimizer, gamma=lr_decay_gamma, verbose=True)
@@ -64,7 +134,7 @@ if __name__ == "__main__":
 
             running_loss += loss.item()
             if (i + 1) % 30 == 0:
-                print('epoch {:3d} | {:5d} batches loss: {:.7f}'.format(epoch, i + 1, running_loss / 30))
+                print('epoch {}|{}  {:5d} batches loss: {:.7f}'.format(epoch, num_epoch, i + 1, running_loss / 30))
                 running_loss = 0.0
                 torch.save(model.state_dict(), weight_save_path)
         scheduler.step()
